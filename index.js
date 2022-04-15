40edf98bab37ab55c346c1ba14fe7edfe428a86d
shivlingbhange2510 <shivlingbhange2510@gmail.com>
2022-04-15 20:36:25 +0530

    new portfolio code

---
+++ index.js
@@ -0,0 +1,16 @@
+// Auto Typed Name
+let typed = new Typed(".auto-typed", {
+    strings: ["Shivling Bhange,"],
+    typeSpeed: 220,
+    backSpeed: 220,
+    loop: true,
+});
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
